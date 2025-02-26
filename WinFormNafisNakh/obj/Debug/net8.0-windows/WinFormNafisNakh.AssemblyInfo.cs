4b90fae0015a81b0f236d8e0ecbd1bf6f0fedad9
Rasol Alibakhshi <63122969+rasolabz2@users.noreply.github.com>
2025-02-26 06:08:48 +0330

    InDependency in Os (win-x64)

--- WinFormNafisNakh/obj/Debug/net8.0-windows/WinFormNafisNakh.AssemblyInfo.cs
+++ WinFormNafisNakh/obj/Debug/net8.0-windows/WinFormNafisNakh.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormNafisNakh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01841652acf69bf3122e7ab906ab4bfd6ac2852b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormNafisNakh")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormNafisNakh")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
